9bb752e6212a72156eb397b2a3f7cc39446dc2b8
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-07-09 07:58:53 -0500

    Convert shell script to batch

--- bin/gitprune.bat
+++ bin/gitprune.bat
@@ -1,3 +1,23 @@
 @echo off
 
-powershell.exe -Command "bash -c 'sh bin/gitprune'"
+setlocal enabledelayedexpansion
+
+set merged=
+
+for /f "tokens=*" %%a in ('git branch --merged ^| findstr /v "*" ^| findstr /v "master" ^| findstr /v "main" ^| findstr /v "develop" ^| findstr /v "trunk"') do (
+  set merged=!merged! %%a
+)
+
+if not defined merged (
+  echo No branches to prune.
+  exit /b 0
+)
+
+echo The following branches will be removed:
+echo %merged%
+echo Press enter to continue or ctrl+c to cancel.
+set /p dummy=
+
+for %%a in (%merged%) do (
+  git branch -d "%%a"
+)
