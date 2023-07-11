734c7e0dfcbf5d710cfca7ca0b733e233a08d375
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-07-10 21:32:40 -0500

    Apply files from wp scaffold theme-tests

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -1,58 +1,59 @@
 <?php
 /**
- * PHPUnit bootstrap file
+ * PHPUnit bootstrap file.
  *
- * @package WordPress Theme
+ * @package Wordpress_Theme
  */
-
-// Debug settings for parity with WordPress Core's PHPUnit tests.
-if ( ! defined( 'WP_DEBUG' ) ) {
-	define( 'WP_DEBUG', true );
-}
-if ( ! defined( 'LOCAL_WP_DEBUG_LOG' ) ) {
-	define( 'LOCAL_WP_DEBUG_LOG', true );
-}
-if ( ! defined( 'LOCAL_WP_DEBUG_DISPLAY' ) ) {
-	define( 'LOCAL_WP_DEBUG_DISPLAY', true );
-}
-if ( ! defined( 'LOCAL_SCRIPT_DEBUG' ) ) {
-	define( 'LOCAL_SCRIPT_DEBUG', true );
-}
-if ( ! defined( 'LOCAL_WP_ENVIRONMENT_TYPE' ) ) {
-	define( 'LOCAL_WP_ENVIRONMENT_TYPE', 'local' );
-}
-
-// Pretend that these are Core unit tests. This is needed so that
-// wp_theme_has_theme_json() does not cache its return value between each test.
-if ( ! defined( 'WP_RUN_CORE_TESTS' ) ) {
-	define( 'WP_RUN_CORE_TESTS', true );
-}
 
 // Require composer dependencies.
 require_once dirname( __DIR__, 2 ) . '/vendor/autoload.php';
 
-// Determine the tests directory (from a WP dev checkout).
-// Try the WP_TESTS_DIR environment variable first.
 $_tests_dir = getenv( 'WP_TESTS_DIR' );
 
-// See if we're installed inside an existing WP dev instance.
 if ( ! $_tests_dir ) {
-	$_try_tests_dir = __DIR__ . '/../../../../../tests/phpunit';
-	if ( file_exists( $_try_tests_dir . '/includes/functions.php' ) ) {
-		$_tests_dir = $_try_tests_dir;
-	}
+	$_tests_dir = rtrim( sys_get_temp_dir(), '/\\' ) . '/wordpress-tests-lib';
 }
-// Fallback.
-if ( ! $_tests_dir ) {
-	$_tests_dir = '/tmp/wordpress-tests-lib';
+
+if ( ! file_exists( $_tests_dir . '/includes/functions.php' ) ) {
+	throw new Error( "Could not find {$_tests_dir}/includes/functions.php, have you run bin/install-wp-tests.sh ?" );
 }
 
 // Give access to tests_add_filter() function.
-require_once $_tests_dir . '/includes/functions.php';
+require_once "{$_tests_dir}/includes/functions.php";
 
-// Do not try to load JavaScript files from an external URL - this takes a
-// while.
-define( 'GUTENBERG_LOAD_VENDOR_SCRIPTS', false );
+/**
+ * Registers theme.
+ */
+function wordpress_theme_register_theme() {
+	$theme_dir     = dirname( __DIR__ );
+	$current_theme = basename( $theme_dir );
+	$theme_root    = dirname( $theme_dir );
+
+	add_filter(
+		'theme_root',
+		function () use ( $theme_root ) {
+			return $theme_root;
+		}
+	);
+
+	register_theme_directory( $theme_root );
+
+	add_filter(
+		'pre_option_template',
+		function () use ( $current_theme ) {
+			return $current_theme;
+		}
+	);
+
+	add_filter(
+		'pre_option_stylesheet',
+		function () use ( $current_theme ) {
+			return $current_theme;
+		}
+	);
+}
+
+tests_add_filter( 'muplugins_loaded', 'wordpress_theme_register_theme' );
 
 /**
  * Adds a wp_die handler for use during tests.
@@ -66,17 +67,17 @@ define( 'GUTENBERG_LOAD_VENDOR_SCRIPTS', false );
  *
  * @throws Exception When a `wp_die()` occurs.
  */
-function fail_if_died( $message ) {
+function wordpress_theme_fail_if_died( $message ) {
 	if ( is_wp_error( $message ) ) {
 		$message = $message->get_error_message();
 	}
 
 	throw new Exception( 'WordPress died: ' . $message );
 }
-tests_add_filter( 'wp_die_handler', 'fail_if_died' );
+tests_add_filter( 'wp_die_handler', 'wordpress_theme_fail_if_died' );
 
 // Start up the WP testing environment.
-require $_tests_dir . '/includes/bootstrap.php';
+require "{$_tests_dir}/includes/bootstrap.php";
 
 // Use existing behavior for wp_die during actual test execution.
 remove_filter( 'wp_die_handler', 'fail_if_died' );
