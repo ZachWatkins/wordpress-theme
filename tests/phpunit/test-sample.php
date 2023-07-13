52f220e439bacd09d820fc37d00791ac707b3dae
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-07-12 21:58:19 -0500

    Test if theme was registered

--- tests/phpunit/test-sample.php
+++ tests/phpunit/test-sample.php
@@ -16,5 +16,7 @@ class SampleTest extends WP_UnitTestCase {
 	public function test_sample() {
 		// Replace this with some actual testing code.
 		$this->assertTrue( true );
+		// Test if the theme is active.
+		$this->assertTrue( 'Wordpress_Theme' === wp_get_theme()->get( 'Name' ) );
 	}
 }
