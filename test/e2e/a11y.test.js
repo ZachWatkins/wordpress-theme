2e3e94bcc662fd86070a1433c5bc8bfffd95eee6
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-07-16 21:28:05 -0500

    Correct the test

--- test/e2e/a11y.test.js
+++ test/e2e/a11y.test.js
@@ -7,6 +7,7 @@ import AxeBuilder from '@axe-core/playwright';
 test( 'should not have automatically detectable accessibility issues', async ( {
 	page,
 }, testInfo ) => {
+	await page.goto( '/' );
 	const accessibilityScanResults = await new AxeBuilder( { page } )
 		.withTags( [ 'wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa' ] )
 		.analyze();
