3ace44fef0ee13adc0571122c73e07151978e94d
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-07-22 15:42:08 -0500

    Modify the test so it passes

--- test/phpunit/TestExample.php
+++ test/phpunit/TestExample.php
@@ -8,12 +8,12 @@ class TestExample extends WP_UnitTestCase {
 
 	public function test_wp_phpunit_is_loaded_via_composer() {
 		$this->assertStringStartsWith(
-			dirname( __DIR__ ) . '/vendor/',
+			dirname( __DIR__, 2 ) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR,
 			getenv( 'WP_PHPUNIT__DIR' )
 		);
 
 		$this->assertStringStartsWith(
-			dirname( __DIR__ ) . '/vendor/',
+			dirname( __DIR__, 2 ) . DIRECTORY_SEPARATOR . 'vendor' . DIRECTORY_SEPARATOR,
 			( new ReflectionClass( 'WP_UnitTestCase' ) )->getFileName()
 		);
 	}
