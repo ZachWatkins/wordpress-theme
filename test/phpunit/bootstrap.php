ab6b1d575adae3fce003a2120f1fd2c7d173d8d8
Zachary K. Watkins <3458875+ZachWatkins@users.noreply.github.com>
2023-07-22 16:27:38 -0500

    Add feature flag for sqlite mode

--- test/phpunit/bootstrap.php
+++ test/phpunit/bootstrap.php
@@ -11,24 +11,52 @@ require_once dirname( __DIR__, 2 ) . '/vendor/autoload.php';
 // Give access to tests_add_filter() function.
 require_once getenv( 'WP_PHPUNIT__DIR' ) . '/includes/functions.php';
 
-// Prepare installation for WordPress SQLite Database Integration plugin.
-if ( ! is_dir( dirname( __DIR__, 2 ) . '/wordpress/wp-content/' ) ) {
-	mkdir( dirname( __DIR__, 2 ) . '/wordpress/wp-content/' );
+// Activate or deactivate WordPress SQLite Database Integration plugin.
+if ( 'enabled' === getenv( 'WP_SQLITE_MODE' ) ) {
+
+	if ( ! is_dir( dirname( __DIR__, 2 ) . '/wordpress/wp-content/' ) ) {
+		mkdir( dirname( __DIR__, 2 ) . '/wordpress/wp-content/' );
+	}
+	file_put_contents(
+		dirname( __DIR__, 2 ) . '/wordpress/wp-content/db.php',
+		str_replace(
+			array(
+				'{SQLITE_IMPLEMENTATION_FOLDER_PATH}',
+				'{SQLITE_PLUGIN}',
+			),
+			array(
+				dirname( __DIR__, 2 ) . '/vendor/wordpress/sqlite-database-integration',
+				'sqlite-database-integration/load.php',
+			),
+			file_get_contents( dirname( __DIR__, 2 ) . '/vendor/wordpress/sqlite-database-integration/db.copy' )
+		)
+	);
+
+	/**
+	 * Activate plugin.
+	 */
+	tests_add_filter(
+		'muplugins_loaded',
+		function () {
+			require dirname( __DIR__, 2 ) . '/vendor/wordpress/sqlite-database-integration/load.php';
+		}
+	);
+
+} else {
+
+	// Undo changes if necessary.
+	if ( file_exists( dirname( __DIR__, 2 ) . '/wordpress/wp-content/db.php' ) ) {
+		unlink( dirname( __DIR__, 2 ) . '/wordpress/wp-content/db.php' );
+		$remaining_files = array_diff(
+			scandir( dirname( __DIR__, 2 ) . '/wordpress/wp-content/' ),
+			array( '..', '.' )
+		);
+		if ( empty( $remaining_files ) ) {
+			rmdir( dirname( __DIR__, 2 ) . '/wordpress/wp-content/' );
+		}
+	}
+
 }
-file_put_contents(
-	dirname( __DIR__, 2 ) . '/wordpress/wp-content/db.php',
-	str_replace(
-		array(
-			'{SQLITE_IMPLEMENTATION_FOLDER_PATH}',
-			'{SQLITE_PLUGIN}',
-		),
-		array(
-			dirname( __DIR__, 2 ) . '/vendor/wordpress/sqlite-database-integration',
-			'sqlite-database-integration/load.php',
-		),
-		file_get_contents( dirname( __DIR__, 2 ) . '/vendor/wordpress/sqlite-database-integration/db.copy' )
-	)
-);
 
 /**
  * Registers theme.
@@ -36,9 +64,6 @@ file_put_contents(
 tests_add_filter(
 	'muplugins_loaded',
 	function () {
-		// Activate WordPress SQLite Database Integration plugin.
-		require dirname( __DIR__, 2 ) . '/vendor/wordpress/sqlite-database-integration/load.php';
-
 		// Activate the theme being developed in this repository.
 		$theme_dir     = dirname( __DIR__, 2 );
 		$current_theme = basename( $theme_dir );
